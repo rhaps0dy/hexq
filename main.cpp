b473aeec594ba7ca3d3c6466c066b1776fa9bf68
Adrià Garriga <adria.garriga@gmail.com>
2016-04-13 21:52:50 +0200

    The taxi works

--- main.cpp
+++ main.cpp
@@ -17,30 +17,26 @@ int main() {
 
 	cout << "Level 1" << endl;
 	HexqLevel lvl_1(1, &lvl_0, &mdp);
-	lvl_1.BuildRegionsExits(TIME);
+	lvl_1.BuildRegionsExitsOrRead("level_1.save", TIME);
 	lvl_1.OutputInfo();
-	ofstream o0("level_0.save");
-	o0 << lvl_0;
 
 	cout << "Level 2" << endl;
 	HexqLevel lvl_2(2, &lvl_1, &mdp);
 	lvl_2.BuildRegionsExitsOrRead("level_2.save", TIME);
 	lvl_2.OutputInfo();
-	return 0;
-}
 
-int sarsa_test_main() {
-	TaxiDomainMdp mdp;
-	mdp.Reset();
-	HexqLevel lvl_0(-1, &mdp, &mdp);
-	lvl_0.BuildRegionsExitsOrRead("level_0.save", TIME);
-	for(int i=0; i<100000; i++) {
+	mdp.frame_time = 100000;
+	for(int i=0; i<10; i++) {
 		mdp.Reset();
-		lvl_0.TakeAction(0);
+		lvl_2.TakeAction(0);
 	}
+
+	ofstream o0("level_0.save");
+	o0 << lvl_0;
+	ofstream o1("level_1.save");
+	o1 << lvl_1;
+	ofstream o2("level_2.save");
+	o2 << lvl_2;
 	printf("\n\n\n\n\n\n\n");
-	printf("\n\n\n\n\n\n\n");
-	ofstream o("level_0.save");
-	o << lvl_0;
 	return 0;
 }
